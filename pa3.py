e339157279fa8c936ed76d10fef10ee7c1c23a08
Joshua Kang <joshuakang@Joshuas-MBP-7.com>
2019-05-15 10:01:15 -0700

    get only ones and twos labels

--- pa3.py
+++ pa3.py
@@ -14,6 +14,28 @@ def loadData(fileName, features=[], labels=[]):
         features.append(currentLine)
         labels.append(line[len(line)-1])
 
+def regularPerceptron(trainingSet, trainingLabels):
+    #default w1
+    w1 = []
+    for i in range (0, 2):
+        w1.append(0)
+
+    for i in range(len(trainingSet)):
+        dotProduct = numpy.dot(numpy.array(trainingSet[i]), w1)
+        if float(trainingLabels[i] ) * float(dotProduct) <= 0:
+            print('update')
+            w1 = numpy.add(w1, numpy.array(trainingSet[i])*int(trainingLabels[i]))
+        print(w1)
+
+def getLabelsOneAndTwo(subset, subsetLabels, trainingSet, trainingLabels):
+    for i in range(len(trainingLabels)):
+        label = trainingLabels[i]
+        if int(label) == 1: 
+            subsetLabels.append(1)
+            subset.append(trainingSet[i])
+        elif int(label) == 2:
+            subsetLabels.append(-1)
+            subset.append(trainingSet[i])
 
 #main 
 trainingSet = []
@@ -21,22 +43,10 @@ trainingLabels = []
 #loadData('pa3train.txt', trainingSet, trainingLabels)
 loadData('practice.txt', trainingSet, trainingLabels)
 
-#default w1
-w1 = []
-for i in range (0, 2):
-    w1.append(0)
+subset = []
+subsetLabels = []
+getLabelsOneAndTwo(subset, subsetLabels, trainingSet, trainingLabels)
 
-#store all the classifiers
-classifiers = []
-classifiers.append(w1)
+regularPerceptron(subset, subsetLabels)
 
-for i in range(len(trainingSet)):
-    dotProduct = numpy.dot(numpy.array(trainingSet[i]), classifiers[i])
-    if float(trainingLabels[i] ) * float(dotProduct) <= 0:
-        print('update')
-        newClassifier = numpy.add(classifiers[-1], numpy.array(trainingSet[i])*int(trainingLabels[i]))
-    else: 
-        newClassifier = classifiers[-1]
-    print(newClassifier)
-    classifiers.append(newClassifier)
 
